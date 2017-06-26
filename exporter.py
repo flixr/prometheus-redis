50d30d564e69e38aa54688e5fc5cc7a27e10211b
Felix Ruess <felix.ruess@roboception.de>
2017-06-26 15:04:11 +0200

    better error handling and logging

--- exporter.py
+++ exporter.py
@@ -8,6 +8,7 @@ import time
 import yaml
 from prometheus_client import start_http_server
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
+import logging
 
 
 class RedisExporter(object):
@@ -19,24 +20,36 @@ class RedisExporter(object):
         for metric_name in self.query['metrics']:
             metric = self.query['metrics'][metric_name]
             if 'query' not in metric:
-                print("Warning: skipping metric '{}' without query".format(metric_name))
+                logging.warning("skipping metric '{}' without query".format(metric_name))
                 continue
             # if connection is not given for a metric, use the "first" we find (not necessarily the first in yaml file)
             conn = metric.get('connection', self.redisConnections.keys()[0])
-            value = self.redisConnections[conn].execute_command(metric['query'])
-            if value is not None:
-                try:
-                    value = float(value)
-                except TypeError:
-                    print("Error: {}: Could not convert value '{}' to float".format(metric_name, value))
-                    continue
-                yield GaugeMetricFamily(
-                    metric.get('name', metric_name),
-                    metric.get('description', ''),
-                    value=value
-                )
+            try:
+                value = self.redisConnections[conn].execute_command(metric['query'])
+            except redis.RedisError as e:
+                logging.error(e)
+                continue
 
-if __name__ == "__main__":
+            if value is None:
+                logging.debug("skipping metric '{}' as query '{}' returned nothing".format(metric_name, metric['query']))
+                continue
+
+            try:
+                value = float(value)
+            except TypeError:
+                logging.error("metric {}: Could not convert value '{}' to float".format(metric_name, value))
+                continue
+
+            yield GaugeMetricFamily(
+                metric.get('name', metric_name),
+                metric.get('description', ''),
+                value=value
+            )
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
     if len(sys.argv) == 0:
         raise "The path of the query file is needed"
 
@@ -44,7 +57,8 @@ if __name__ == "__main__":
         try:
             query = yaml.load(yaml_query)['prometheus_redis']
         except yaml.YAMLError as e:
-            print(e)
+            logging.error(e)
+            sys.exit(1)
 
     redisConnections = {}
     for conn in query['connections']:
@@ -58,4 +72,10 @@ if __name__ == "__main__":
 
     REGISTRY.register(RedisExporter(query, redisConnections))
     start_http_server(query.get('server', {}).get('port', 9118))
-    while True: time.sleep(1)
+
+    while True:
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
